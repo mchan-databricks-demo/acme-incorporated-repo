82e923c103c7158ce639f4d3d5384b1d42f809dd
mchan-databricks-demo <mchan-databricks-demo>
2022-09-06 04:58:00 +0000

    1st-commit-in-dev-branch

---
+++ 01-nb-script.py
@@ -0,0 +1,13 @@
+# Databricks notebook source
+s3Path = "dbfs:/mnt/mchan_superstore/orders”
+df = spark.read
+	.format("csv")
+	.option(“sep”, “\t”)
+  	.option(“encoding”, “UTF-8”)
+	.option("header", ”true")
+	.option("dateFormat", "yyyy-M-dd")
+           .option(“timestampFormat”, “yyyy-MM-dd HH:mm:ss”)
+	.option("inferSchema", "true")
+        .withColumn(“fname”, input_file_name())
+        .withColumn(“ingesttime”, current_timesstamp())
+	.load(s3Path) 
